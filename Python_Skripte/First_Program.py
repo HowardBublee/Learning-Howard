5b6005418b307e6665b018f9084af6821917dd70
Oli_S <oschmidt.nbg@gmail.com>
2025-04-16 10:27:09 +0200

    Uebung_Klasse

--- Python_Skripte/First_Program.py
+++ Python_Skripte/First_Program.py
@@ -1,1 +1,22 @@
-print("Test")
+
+class Auto:
+    def __init__(self,Marke,Modell,BJ):
+        self.marke=Marke
+        self.Modell=Modell
+        self.BJ=BJ
+        self.URL= "URL"
+    
+p=Auto("Audi","A6","1969")
+print(p.marke)
+print(p.BJ)
+
+class Elektroauto(Auto):
+    def __init__(self, Marke, Modell, BJ,MotorLeistung,Reichweite):
+        super().__init__(Marke, Modell, BJ)
+        
+        self.Motorleistung=MotorLeistung
+        self.Reichweite=Reichweite
+        
+e_Auto=Elektroauto("Audi","A6","1969","200W","1600")
+print(e_Auto.URL)
+print(e_Auto.marke)      
